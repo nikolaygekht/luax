04c5d387ea3a7db0ad11aa74e19e42501b17df78
Ivan Petrovich <71124614+ipetrovich86@users.noreply.github.com>
2021-11-26 21:20:51 +0600

    Added FOR STM (#23)
    Add FOR statement
    
    Co-authored-by: ydergachev-gsg <ydergachev@gehtsoft.com>

---
+++ Luax.Parser/Ast/Statement/LuaXForLoopDescription.cs
@@ -0,0 +1,28 @@
+﻿using Luax.Parser.Ast.LuaExpression;
+using System;
+
+/// <summary>
+/// FOR LOOP statement
+/// </summary>
+namespace Luax.Parser.Ast.Statement
+{
+    public class LuaXForLoopDescription
+    {
+        public LuaXExpression Start { get; }
+
+        public LuaXExpression Limit { get; }
+
+        public LuaXExpression Step { get; }
+
+        public LuaXVariable Variable { get; }
+
+        internal LuaXForLoopDescription(LuaXVariable identierVar, LuaXExpression start,
+            LuaXExpression limit, LuaXExpression step)
+        {
+            Variable = identierVar;
+            Start = start;
+            Limit = limit;
+            Step = step;
+        }
+    }
+}
